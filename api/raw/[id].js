517f9ca516a9fc77767f8fe348944d456cf8b14f
Shrihan Kaushik Kandagatla <198945176+QubitGates@users.noreply.github.com>
2026-04-24 20:24:44 +0530

    .

--- api/raw/[id].js
+++ api/raw/[id].js
@@ -1,16 +1,22 @@
 import redis from "../../lib/redis.js";
 
 export default async function handler(req, res) {
-    const { id } = req.query;
+    try {
+        const { id } = req.query;
 
-    const data = await redis.get(id);
+        const data = await redis.get(id);
 
-    if (!data) {
-        return res.status(404).send("Expired or invalid");
+        if (!data) {
+            return res.status(404).send("Expired or invalid");
+        }
+
+        await redis.del(id);
+
+        res.setHeader("Content-Type", "text/plain");
+        res.send(data.text);
+
+    } catch (err) {
+        console.error("RAW ERROR:", err);
+        res.status(500).send("Server error");
     }
-
-    await redis.del(id);
-
-    res.setHeader("Content-Type", "text/plain");
-    res.send(data.text);
 }
--- api/view/[id].js
+++ api/view/[id].js
@@ -1,41 +1,45 @@
 import redis from "../../lib/redis.js";
 
 export default async function handler(req, res) {
-    const { id } = req.query;
+    try {
+        const { id } = req.query;
 
-    const data = await redis.get(id);
+        if (!id) {
+            return res.status(400).send("Invalid ID");
+        }
 
-    if (!data) {
-        return res.status(404).send("Expired or invalid");
+        const data = await redis.get(id);
+
+        if (!data) {
+            return res.status(404).send("Expired or invalid");
+        }
+
+        await redis.del(id);
+
+        if (data.stealth) {
+            return res.send(`
+        <html>
+          <body style="margin:0;">
+            <script>
+              const text = ${JSON.stringify(data.text)};
+              document.addEventListener("click", async () => {
+                try {
+                  await navigator.clipboard.writeText(text);
+                  document.body.innerHTML = "Copied";
+                } catch {
+                  document.body.innerHTML = "<pre>"+text+"</pre>";
+                }
+              });
+            </script>
+          </body>
+        </html>
+      `);
+        }
+
+        res.send(`<pre>${data.text}</pre>`);
+
+    } catch (err) {
+        console.error("VIEW ERROR:", err);
+        res.status(500).send("Server error");
     }
-
-    await redis.del(id);
-
-    if (data.stealth) {
-        return res.send(`
-      <html>
-        <body style="margin:0;background:#fff;">
-          <script>
-            const text = ${JSON.stringify(data.text)};
-            document.addEventListener("click", async () => {
-              try {
-                await navigator.clipboard.writeText(text);
-                document.body.innerHTML = "<div style='text-align:center;margin-top:40vh;font-family:sans-serif;'>Copied</div>";
-              } catch {
-                document.body.innerHTML = "<pre>"+text+"</pre>";
-              }
-            });
-          </script>
-        </body>
-      </html>
-    `);
-    }
-
-    res.send(`
-    <html>
-      <body style="padding:40px;font-family:sans-serif;">
-        <pre>${data.text}</pre>
-      </body>
-    </html>
-  `);
 }
