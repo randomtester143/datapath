cd3b0a65786c7674c8c7445d772fd2497d243f8c
Shrihan Kaushik Kandagatla <198945176+QubitGates@users.noreply.github.com>
2026-04-25 22:43:19 +0530

    .

--- api/raw/[id].js
+++ api/raw/[id].js
@@ -1,6 +1,7 @@
 import { redis } from '../../lib/redis.js';
+import { verifyAndDecrypt, PAYLOAD_VERSION } from '../../lib/crypto.js';
 
 export default async function handler(req, res) {
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    return res.status(200).send('test with redis\n');
+    return res.status(200).send('test with crypto\n');
 }
