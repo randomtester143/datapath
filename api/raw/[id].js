2d37efb805660dd5e9996db542e44012e970e0e0
Shrihan Kaushik Kandagatla <198945176+QubitGates@users.noreply.github.com>
2026-04-25 22:42:02 +0530

    .

--- api/raw/[id].js
+++ api/raw/[id].js
@@ -1,4 +1,6 @@
+import { redis } from '../../lib/redis.js';
+
 export default async function handler(req, res) {
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    return res.status(200).send('test\n');
+    return res.status(200).send('test with redis\n');
 }
